4269ecd10a0afba279e30df44f4a8bf556bd2de4
bWFuanVzYWth <origin_0110@outlook.com>
2023-04-07 11:51:57 +0800

    修复了一个空格导致的语法错误，非常傻逼

--- 双击更新蓝图仓库.bat
+++ 双击更新蓝图仓库.bat
@@ -12,10 +12,10 @@ echo INF: %date% %time% Start>>%LOG_PATH%
 ::find git
 if exist ".\MinGit\cmd\git.exe" (
 set GIT_PATH=.\MinGit\cmd\git.exe
-echo INF: %date% %time% GIT_PATH=MinGit>>%LOG_PATH%
+echo INF: %date% %time% GIT_PATH=.\MinGit\cmd\git.exe>>%LOG_PATH%
 ) else if exist "C:\Program Files\Git" (
 set GIT_PATH=git
-echo INF: %date% %time% GIT_PATH=Git>>%LOG_PATH%
+echo INF: %date% %time% GIT_PATH=git>>%LOG_PATH%
 ) else (
 echo 警告：无法找到Git或MinGit。如果更新能正常进行请忽略 | Warning: Could not find Git or MinGit. Please ignore if the update works normally
 echo WAR: %date% %time% Git/MinGit no found>>%LOG_PATH%
@@ -32,10 +32,11 @@ echo INF: %date% %time% git pull start>>%LOG_PATH%
 git config core.longpaths true
 set GIT_SSL_NO_VERIFY=true
 %GIT_PATH% pull origin main
-set git_pull_errorlevel = %errorlevel%
-echo INF: %date% %time% git pull exit: %git_pull_errorlevel%>>%LOG_PATH%
-if %git_pull_errorlevel% NEQ 0(
-echo 错误：更新失败，这通常是网络问题。请重试，或者开加速器再更新。详见README.md
+if %errorlevel% NEQ 0 (
+echo ERR: %date% %time% git pull error>>%LOG_PATH%
+echo 错误：更新失败，这通常是网络问题。请重试，或者开加速器再更新。详见"README.md"
+) else (
+echo INF: %date% %time% git pull successed>>%LOG_PATH%
 )
 
 ::end
